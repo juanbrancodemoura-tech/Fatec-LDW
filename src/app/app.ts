f572f36c939ad5defa401e7b0f72fff3f830ca60
Juan B. Moura <juanbrancodemoura@gmail.com>
2026-09-09 18:01:08 -0300

    modifi. app.html, app.ts,home.html e home.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Projeto-Angular');
+  protected readonly title = signal('Fatec');
 }
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  protected title = "Bem-Vindos ao Angular Moderno";
+}
